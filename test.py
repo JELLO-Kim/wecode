15f30d2293f300254e2f8129ca7881e03aa6483a
JELLO-Kim <cogus950403@gmail.com>
2021-01-29 14:52:55 +0900

    REVISE

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 def even_num():
     even_list = []
-    for num in range(0 : 51):
+    for num in range(0, 51):
         if num % 2 == 0:
             even_list.append(num)
     return even_list
+
 
